eab004ddc2fe76b2401bd74b74bcf0752454137a
Jason Barnett <jason.w.barnett@gmail.com>
2015-01-09 16:56:19 -0800

    added users

---
+++ gen_users_html.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+require 'yaml'
+
+users = YAML.load_file('users.yml')
+
+html = File.open('users.html', 'w')
+users["users"].each do |user|
+  name = user["name"]
+  html.puts %Q{<li class="col-md-6"><i class="fa fa-user"></i> #{name}</li></li>}
+end
+html.close
+
+
---
+++ gen_users_yaml.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require 'yaml'
+
+users = File.read('users.txt').split("\n")
+
+users = users.inject([]) do |memo,user|
+  memo << {"name" => user, "rank" => "unranked"}
+  memo
+end
+
+users = ({"users" => users}).to_yaml
+
+File.open('users.yml', 'w') {|f| f.write(users) }
+
